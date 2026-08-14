330fa29c184d44361dc942beaf6f8487e6da10f3
Venkat <venkyemmato@gmail.com>
2026-08-14 22:32:00 +0530

    fast api - api call -get & post, with pydantic

--- main_fast_api.py
+++ main_fast_api.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import uvicorn
 
 app = FastAPI(title ="my first api call")
 
+class User(BaseModel):
+    username : str
+    email:str
+    age:int
+
 @app.get("/first_endpoint")
 async def first_endpoint():
     return {"response" : "Hello_world"}
+
+@app.post("/user/registration")
+async def user_register(user_details:User):
+    msg = f"{user_details.username} is registered successfully...!"
+    return msg 
 
 if __name__ == "__main__":
     uvicorn.run(
